6c1ce6cd9f0021a0a29158bf57ac469c7f30654e
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-11-05 22:49:14 +0100

    Connection pool update
    Fixes the exact pool’s capacity. Simplifies the insert method.

--- src/pool.rs
+++ src/pool.rs
@@ -4,24 +4,21 @@ use std::net::SocketAddr;
 use async_std::sync::Mutex;
 
 pub struct ConnectionPool {
+    capacity: usize,
     inner: Mutex<HashSet<SocketAddr>>,
 }
 
 impl ConnectionPool {
     pub fn with_capacity(capacity: usize) -> ConnectionPool {
         ConnectionPool {
+            capacity, // NOTE: HashSet::with_capacity only garantees the least capacity
             inner: Mutex::new(HashSet::with_capacity(capacity)),
         }
     }
 
     pub async fn insert(&self, addr: SocketAddr) -> bool {
         let mut guard = self.inner.lock().await;
-
-        if guard.len() >= guard.capacity() {
-            return false;
-        }
-
-        guard.insert(addr)
+        guard.len() < self.capacity && guard.insert(addr)
     }
 
     pub async fn remove(&self, addr: &SocketAddr) -> bool {
