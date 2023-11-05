57c4c597572c607182209c2c87bbabf0dcb0923e
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-11-05 22:58:11 +0100

    Refactoring
    Unifies incoming connection streams. As a result, removes the CPU usage hack and the per-listener tasks. Moves all related functionality back into the main module. Updates Cargo.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod args;
 mod pool;
-mod tasks;
 
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use async_signals::Signals;
+use async_std::io::ErrorKind;
+use async_std::net::TcpListener;
+use async_std::net::{Shutdown, TcpStream};
 use async_std::prelude::*;
+use async_std::stream::repeat_with;
 use async_std::task;
+use fastrand::alphanumeric;
+use futures::stream::select_all;
 use once_cell::sync::Lazy;
 
 use crate::args::Args;
 use crate::pool::ConnectionPool;
-use crate::tasks::listen;
 
 #[cfg(target_env = "musl")]
 #[global_allocator]
@@ -19,6 +23,52 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 static ARGS: Lazy<Args> = Lazy::new(Args::parse);
 static POOL: Lazy<ConnectionPool> = Lazy::new(|| ConnectionPool::with_capacity(ARGS.cap.into()));
+
+const CRLF: &str = "\r\n";
+
+async fn process(mut stream: TcpStream) {
+    let addr = match stream.peer_addr() {
+        Ok(addr) => addr,
+        Err(_) => return,
+    };
+
+    let length = usize::from(ARGS.length);
+    let cap = length + CRLF.len();
+
+    match POOL.insert(addr).await {
+        true => {
+            println!("Got connection from {addr}");
+
+            let mut buf: String = String::with_capacity(cap);
+            let now = Instant::now();
+
+            loop {
+                repeat_with(alphanumeric)
+                    .take(length)
+                    .for_each(|ch| buf.push(ch))
+                    .await;
+
+                buf.push_str(CRLF);
+
+                if let Some(ref e) = stream.write_all(buf.as_bytes()).await.err() {
+                    if e.kind() != ErrorKind::WouldBlock {
+                        println!("{addr} has gone yet wasted ~{:.2?}", now.elapsed());
+                        break;
+                    }
+                }
+
+                buf.clear();
+
+                task::sleep(Duration::from_millis(ARGS.delay)).await;
+            }
+        }
+        false => {
+            stream.shutdown(Shutdown::Both).ok();
+        }
+    }
+
+    POOL.remove(&addr).await;
+}
 
 #[async_std::main]
 async fn main() {
@@ -32,12 +82,34 @@ async fn main() {
         }
     });
 
-    for addr in ARGS.addrs() {
-        task::spawn(listen(addr));
+    let addrs = ARGS.addrs();
+    let mut listeners = Vec::with_capacity(addrs.capacity());
+
+    for addr in addrs {
+        match TcpListener::bind(addr).await {
+            Ok(listener) => {
+                println!("Listening on {addr}");
+                listeners.push(listener);
+            }
+            Err(ref e) => {
+                eprintln!("Cannot listen on {addr}: {e}");
+                std::process::exit(1);
+            }
+        };
     }
 
-    loop {
-        // HACK: drastically lowers the CPU usage of an infinite loop
-        task::sleep(Duration::from_secs(60)).await;
+    let mut incoming = select_all(listeners.iter().map(|l| l.incoming()));
+
+    while let Some(stream) = incoming.next().await {
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(ref e) => {
+                eprintln!("Cannot obtain a TCP stream: {e}");
+                continue;
+            }
+        };
+        stream.set_nodelay(true).ok(); // we do not really care if it clicks or not
+
+        task::spawn(process(stream));
     }
 }
